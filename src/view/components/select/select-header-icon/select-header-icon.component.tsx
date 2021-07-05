5dd2d59ed33ff6988e276517751a16fd72d3f74e
Vladislav <ivanovlad2010@gmail.com>
2021-07-05 10:31:53 +0300

    select header icon fix

--- src/view/components/select/select-header-icon/select-header-icon.component.tsx
+++ src/view/components/select/select-header-icon/select-header-icon.component.tsx
@@ -14,7 +14,7 @@ export function SelectHeaderIcon({
 
     return (
         <div className={classNames(className, s.self)}>
-            {open ? <ArrowIcon /> : <CloseIcon />}
+            {open ? <CloseIcon /> : <ArrowIcon />}
         </div>
     );
 }
